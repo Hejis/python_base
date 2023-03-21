6b35146be195a5c288d160eda455cfec4012acd3
simpaa_comp <simpaa@inbox.ru>
2023-03-21 15:44:50 +0300

    dz9_all

--- dz8.py
+++ dz8.py
@@ -23,7 +23,5 @@ def read_last(lines, file):
         else:
             print('Количество строк может быть только целым положительным')
  
- 
-# Тесты
 read_last(3, 'article.txt')
 read_last(-5, 'article.txt')
